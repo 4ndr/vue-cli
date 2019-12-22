66cf164b4d891766702397d15614eaaf528d0776
4ndr <andrepereiraleite@gmail.com>
2019-12-21 22:17:09 -0300

    First commit
    Signed-off-by: 4ndr <andrepereiraleite@gmail.com>

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reser Name</button>
+        <button @click="resetFn()">Reser Name</button>
+    </div>
+</template>
+
+<script>
+    import { eventBus } from "../main";
+
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            resetFn: Function,
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = "Maa";
+                this.$emit('nameWasReset', this.name);
+            }
+        },
+        created() {
+            eventBus.$on("ageWasEdited", (age) => {
+                this.userAge = age;
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
